73ad0c2f56f0de2fcb0fb73a595082d10c939b0d
sacry- <nitsche25@gmail.com>
2017-05-23 18:18:33 +0200

    post func or pred

--- pred3.m
+++ pred3.m
@@ -1,9 +1,9 @@
 function [ pred3 ] = pred3(t, y, o)
-    f = y(1); w = y(2); r = y(3); tu = y(4);
-    % Fox 
-    fox_pop = - o.f_dec * f + (o.f_grow * f * r) + (o.f_grow * f * tu);
+    f = o.f(y(1)); w = o.f(y(2)); r = o.f(y(3)); tu = o.f(y(4));
     % Wolf
     wolf_pop = - o.w_dec * w + (o.w_grow * w * r) + (o.w_grow * w * tu);
+    % Fox 
+    fox_pop = - o.f_dec * f + (o.f_grow * f * r) + (o.f_grow * f * tu) - (o.w_grow * w);
     % Rabbit
     rabbit_pop = o.r_grow * r - ((o.r_dec * f * r) + (o.r_dec * w * r));
     % Turkey
--- runner.m
+++ runner.m
@@ -11,15 +11,16 @@ o = struct;
 o.f_dec = 0.5;
 o.f_grow = 0.01;
 o.w_dec = 0.5;
-o.w_grow = 0.011;
+o.w_grow = 0.01;
 o.r_grow = 0.8;
 o.r_dec = 0.01;
 o.tu_grow = 0.8;
 o.tu_dec = 0.01;
+o.f = @(u) ceil(max(2, u));
 
 y0 = [foxes wolves rabbits turkey];
 options = odeset('RelTol', 1e-5);
-[t, y] = ode45(@(t,y) pred3(t,y,o),[0 100],y0,options); 
+[t, y] = ode45(@(t,y) pred3(t,y,o),[0 30],y0,options); 
 
 f = y(:,1);
 w = y(:,2);
@@ -32,5 +33,5 @@ a2 = plot(t,w); M2 = "Wolf";
 a3 = plot(t,r); M3 = "Rabbit";
 a4 = plot(t,tu); M4 = "Turkey";
 legend([a1,a2,a3,a4], [M1, M2, M3, M4]);
-ylabel('foxes/wolves + rabbits/turkeys')
-xlabel('time')
+ylabel('Populations')
+xlabel('Time')
